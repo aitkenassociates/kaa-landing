668d86bde1e70bd6c5d82bf8ec49b452d380c96a
Daniel García <dany.el553@gmail.com>
2025-05-21 18:26:13 -0600

    Add first landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,63 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import mdx from "@astrojs/mdx";
 import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
+import AutoImport from "astro-auto-import";
+import remarkCollapse from "remark-collapse";
+import remarkToc from "remark-toc";
+import config from "./src/config/config.json";
+
+let highlighter;
+async function getHighlighter() {
+  if (!highlighter) {
+    const { getHighlighter } = await import("shiki");
+    highlighter = await getHighlighter({ theme: "one-dark-pro" });
+  }
+  return highlighter;
+}
 
 // https://astro.build/config
 export default defineConfig({
+    site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+    base: config.site.base_path ? config.site.base_path : "/",
+    trailingSlash: config.site.trailing_slash ? "always" : "never",
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [react()],
+    integrations: [
+        react(),
+        sitemap(),
+        AutoImport({
+          imports: [
+            "@/shortcodes/Button",
+            "@/shortcodes/Accordion",
+            "@/shortcodes/Notice",
+            "@/shortcodes/Video",
+            "@/shortcodes/Youtube",
+            "@/shortcodes/Tabs",
+            "@/shortcodes/Tab",
+          ],
+        }),
+        mdx(),
+    ],
+    markdown: {
+        remarkPlugins: [
+          remarkToc,
+          [
+            remarkCollapse,
+            {
+              test: "Table of contents",
+            },
+          ],
+        ],
+        shikiConfig: {
+          theme: "one-dark-pro",
+          wrap: true,
+        },
+        extendDefaultPlugins: true,
+        highlighter: getHighlighter,
+    },
     adapter: netlify()
 });
